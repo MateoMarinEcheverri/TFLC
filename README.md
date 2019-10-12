1e166e7d52a9bb009c675eafbb54dedb2bd8a8fc
Julian Santiago Tauta <julianstauta@gmail.com>
2019-10-12 15:14:25 -0500

    Baseline

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # TFLC
 Repositorio para el  Taller Frameworks del Lado del cliente, Curso de programacion web
+
+# tallerfwjs
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  
+  <v-app>
+  <div>
+    <v-app-bar
+      color="black"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-toolbar-items>
+        <v-btn text to="/home">Home</v-btn>
+        <v-btn text to="/signup">Sign Up</v-btn>
+        <v-btn text to="/signin">Sign In</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
+    
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    
+  </v-app>
+
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data(){
+    return {
+      appTitle: 'Awesome App',
+      sidebar: false,
+      menuItems: [
+          { title: 'Home', path: '/home', icon: 'home' },
+          { title: 'Sign Up', path: '/signup', icon: 'face' },
+          { title: 'Sign In', path: '/signin', icon: 'lock_open' }
+     ]
+    }
+  },
+};
+</script>
+<style>
+
+</style>
